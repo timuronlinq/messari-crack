a899c78aa38434e0845eda934fc0c2f9f00d56ed
Alexey Nurmukhametov <miscompgradiner@outlook.com>
2026-02-07 21:49:52 +0700

    update

---
+++ src/components/__init__.py
@@ -0,0 +1,4 @@
+from components.header import HeaderPanel
+from components.selector_panel import SelectorPanel
+from components.control_panel import ControlPanel
+from components.log_console import LogConsole
